7604a603063b24bf1dac3e813f91b3a08487370d
HARIKIRAN MADISHETTI <madishetti@HARIKIRANs-MacBook-Pro.local>
2021-02-24 18:02:30 -0600

    Added function fetchincidents() to download data from url

--- project0/project0.py
+++ project0/project0.py
@@ -3,3 +3,12 @@ import tempfile
 import PyPDF2
 import sqlite3
 import re
+
+#Downloading Data
+
+def fetchincidents(url):
+    headers = {}
+    headers['User-Agent'] = "Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.17 (KHTML, like Gecko) Chrome/24.0.1312.27 Safari/537.17"                          
+    data = urllib.request.urlopen(urllib.request.Request(url, headers=headers))
+
+    return data
