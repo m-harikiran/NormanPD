54a17501c4132f8dc1ba2d0e4c1598c67f985651
HARIKIRAN MADISHETTI <madishetti@HARIKIRANs-MacBook-Pro.local>
2021-02-24 18:46:59 -0600

    Added function extractincidents() to extract data

--- project0/project0.py
+++ project0/project0.py
@@ -12,3 +12,43 @@ def fetchincidents(url):
     data = urllib.request.urlopen(urllib.request.Request(url, headers=headers))
 
     return data
+
+def extractincidents(incident_data):
+    
+    #Creating a temporary file to store the data
+    fp = tempfile.TemporaryFile()
+
+    # Write the pdf data to a temp file
+    fp.write(incident_data.read())
+
+    #setting the cursor of the file to start
+    fp.seek(0)
+
+    #Reading the PDF
+    pdfReader = PyPDF2.pdf.PdfFileReader(fp)
+    pageNumbers = pdfReader.getNumPages()   #Getting the total page numbers in PDF
+
+    pdfData = pdfReader.getPage(0).extractText() #Extracting the text from page
+
+    #Iterating through remaining pages and appending data
+    for i in range (1, pageNumbers):
+        pdfData += pdfReader.getPage(i).extractText()
+    
+    #Cleaning the data extracted from pdf
+
+    pdfData = pdfData.replace(' \n', ' ')  #Handling columns with multi-line data
+
+    pdfData = re.split( r'\s+(?=\d?\d?\/\d?\d?\/\d{4} \d?\d?:\d?\d?)' , pdfData) #Spliting the data based on date
+
+    pdfDataList = []  
+    
+    for i in range(1, len(pdfData)-1):
+        l = pdfData[i].split('\n')
+
+        if len(l) > 5:
+            l.pop()
+        
+        pdfDataList.append(l)
+    
+    return pdfDataList
+
